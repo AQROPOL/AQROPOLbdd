bcabac075963c2e8023fc67631c9f71c15be6817
adrien leblanc <17000952@e212m06.istic.univ-rennes1.fr>
2018-02-07 15:18:34 +0100

    Test de modification encodage json

--- www/API/v2/donnees.php
+++ www/API/v2/donnees.php
@@ -8,6 +8,7 @@
 			$stmt_readMeta->execute();
 			$tabMeta = $stmt_readMeta->fetchAll(PDO::FETCH_ASSOC);
 			$i = 0;
+			// Ici on lie chaque "mesures" a la "meta_mesures" qui lui correspond
 			foreach($tabMeta as &$meta){
 				$mesures = array();
 				$i_mesure = 0;
@@ -21,6 +22,33 @@
 				$i++;
 			}
 			$result = array("meta_mesures" => $meta_mesures);
-			echo json_encode($result);
+			////////////
+			$tabMatchWebMapping = array();
+			$tabMatchWebMapping["type"] = "FeatureCollection";
+			$tabFeatures = array();
+			$i = 0;
+			foreach ($mesure as $result) {
+				$tabFeatures["type"] = "Feature";
+
+				$tabFeaturesProperties = array();
+				$tabFeaturesProperties["id_mesure"]	= $result["meta_mesures"][$i++]["mesures"]["id"];
+				$tabFeaturesProperties["id_capteur"]	= $result["meta_mesures"][$i++]["mesures"]["id_capteur"];
+				$tabFeaturesProperties["valeur"]		= $result["meta_mesures"][$i++]["mesures"]["valeur"];
+				$tabFeaturesProperties["type"]		= $result["meta_mesures"][$i++]["mesures"]["type"];
+				$tabFeaturesProperties["id_meta"] 		= $result["meta_mesures"][$i++]["id"];
+				$tabFeaturesProperties["id_hub"] 		= $result["meta_mesures"][$i++]["id_hub"];
+				$tabFeaturesProperties["date"] 		= $result["meta_mesures"][$i++]["date"];
+				$tabFeatures["properties"] = $tabFeaturesProperties;
+
+				$tabFeaturesGeometry = array();
+				$tabFeaturesGeometry["type"] = "Point";
+				$tabFeaturesGeometryCoordinates = array();
+				$tabFeaturesGeometryCoordinates[0] = $result["meta_mesures"][$i++]["gps_lat"];
+				$tabFeaturesGeometryCoordinates[1] = $result["meta_mesures"][$i++]["gps_long"];
+				$tabFeaturesGeometry["coordinates"] = $tabFeaturesGeometryCoordinates;
+				$tabFeatures["geometry"] = $tabFeaturesGeometry;
+			}
+			$tabMatchWebMapping["features"] = $tabFeatures;
+			echo json_encode($tabMatchWebMapping);
 	}
 ?>
