e7c555bdc800e0cd80c3586d39a3b549f01464f7
noureddine kadri <16012172@e010m05.istic.univ-rennes1.fr>
2018-03-14 15:49:08 +0100

    Récuperation du Json

---
+++ www/API/v2/JsonToMysql.php
@@ -0,0 +1,61 @@
+<?php
+	require 'db_access.php';
+	
+	header("Content-type: application/json");
+
+	if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+		$json_data = file_get_contents('data.json');
+
+		$data=json_decode($json_data,true);
+
+		$id_nuc=0;
+		$id_capteur=0;
+		$id_meta=0;
+		$nuc = $data[nuc];
+		$stmt_insertHubs -> bindParam(':name',$nuc);
+		$stmt_insertHubs -> execute();
+	
+	while ($row = $stmt_insertHubs->fetch(PDO::FETCH_ASSOC)) {
+		$id_nuc = $row['id'];
+		}
+
+		$mesures=$data["mesures"];
+		$max = sizeof($mesures);
+	
+	
+	for ($i = 0; $i < $max; $i++) {
+	
+		$stmt_insertCapteurs -> bindParam(':id_hub',$id_nuc);
+		$stmt_insertCapteurs -> bindParam(':name',$mesures[i]["capteur"]);
+		$stmt_insertCapteurs -> bindParam(':type',$mesures[i]["type"]);
+		$stmt_insertCapteurs -> execute();
+	
+	while ($row = $stmt_insertCapteurs ->fetch(PDO::FETCH_ASSOC)) {
+		$id_capteur = $row['id'];
+		}
+	
+		$stmt_insertMetaMesures ->bindParam(':id_hub',$id_nuc);
+		$stmt_insertMetaMesures ->bindParam(':date',$mesures[i]["date"]);
+		$stmt_insertMetaMesures ->bindParam(':gps_lat',$mesures[i]["lat"]);
+		$stmt_insertMetaMesures ->bindParam(':gps_long',$mesures[i]["long"]);
+		$stmt_insertMetaMesures ->bindParam(':hash',$mesures[i]["hash"]);
+		$stmt_insertMetaMesures -> execute();
+	
+	 while ($row = $stmt_insertMetaMesures ->fetch(PDO::FETCH_ASSOC)) {
+		$id_meta = $row['id'];
+ 
+		}
+	
+	
+	
+		$stmt_insertMesures->bindParam(':id_capteur',$id_capteur);
+		$stmt_insertMesures->bindParam(':id_meta',$id_meta);
+		$stmt_insertMesures->bindParam(':valeur',$mesures[i]["valeur"]);
+		$stmt_insertMesures->execute();
+	
+	}
+
+	}
+
+?>
