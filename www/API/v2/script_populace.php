acc54a87b6b64d729efe68e3dc768c90756b9fd4
AdrienLeblanc <adrienleblanc53@gmail.com>
2018-03-14 15:32:45 +0100

    script_populace.php

--- www/API/v2/script_populace.php
+++ www/API/v2/script_populace.php
@@ -2,16 +2,21 @@
 
 require 'db_access.php';
 
-for ($i = 0; $i < 1000; $i++) {
+for ($i = 1; $i < 1000; $i++) {
      $aleaLat = rand(4802, 4820);
      $aleaLat = $aleaLat / 100;
      $aleaLong = rand(-144, -184);
      $aleaLong = $aleaLong / 100;
 
-     echo "lat = ". $aleaLat . " long = " . $aleaLong;
      $date = new DateTime();
      $dateTmp = $date->getTimestamp();
-     $rand2 = rand(1,2);
-     $stmt_insertMetaMesures->execute(array(':id_hub' => $rand2, ':date' => $dateTmp, ':gps_long' => $aleaLong, ':gps_lat' => $aleaLat));
+     $randIdHub = rand(1,2);
+     $stmt_insertMetaMesures->execute(array(':id_hub' => $randIdHub, ':date' => $dateTmp, ':gps_long' => $aleaLong, ':gps_lat' => $aleaLat));
+
+     $randIdCapteur = rand(1,4);
+     $randValeur = rand(0,10000);
+
+     $stmt_insertMesures->execute(array(':id_capteur' => $randIdCapteur, ':id_meta' => $i, ':valeur' => $randValeur));
+
 }
  ?>
