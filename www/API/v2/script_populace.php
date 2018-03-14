7ce80dfa9fac095911393309a97054f1928359c0
AdrienLeblanc <adrienleblanc53@gmail.com>
2018-03-14 15:42:03 +0100

    script_populace.php

--- www/API/v2/script_populace.php
+++ www/API/v2/script_populace.php
@@ -11,7 +11,8 @@ for ($i = 1; $i < 1000; $i++) {
      $date = new DateTime();
      $dateTmp = $date->getTimestamp();
      $randIdHub = rand(1,2);
-     $stmt_insertMetaMesures->execute(array(':id_hub' => $randIdHub, ':date' => $dateTmp, ':gps_long' => $aleaLong, ':gps_lat' => $aleaLat, ":hash" => hash('sha256', $dateTmp)));
+     $hash = hash('sha256', $dateTmp);
+     $stmt_insertMetaMesures->execute(array(':id_hub' => $randIdHub, ':date' => $dateTmp, ':gps_long' => $aleaLong, ':gps_lat' => $aleaLat, ":hash" => $hash));
 
      $randIdCapteur = rand(1,4);
      $randValeur = rand(0,10000);
