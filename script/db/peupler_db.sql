62439e9e10dfaf087b56ecf9dbeacdf0c10d4f35
AdrienLeblanc <adrienleblanc53@gmail.com>
2018-03-14 14:59:18 +0100

    changement script peupler_db.sql

--- script/db/peupler_db.sql
+++ script/db/peupler_db.sql
@@ -15,20 +15,21 @@ INSERT INTO hubs(name) VALUES("centre_rennes");
 INSERT INTO hubs(name) VALUES("istic");
 
 /* On peuple capteurs */
-INSERT INTO capteurs(id_hub,type) VALUES(1,"particules");
-INSERT INTO capteurs(id_hub,type) VALUES(2,"particules");
-INSERT INTO capteurs(id_hub,type) VALUES(1,"meteo");
-INSERT INTO capteurs(id_hub,type) VALUES(2,"meteo");
+INSERT INTO capteurs(id_hub,name,type) VALUES(1,"capteur1","particules");
+INSERT INTO capteurs(id_hub,name,type) VALUES(2,"capteur2","particules");
+INSERT INTO capteurs(id_hub,name,type) VALUES(1,"capteur3","bus");
+INSERT INTO capteurs(id_hub,name,type) VALUES(2,"capteur4","bus");
 
 /* On peuple meta_mesures */
-INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(1, 1508666400,    48.1151495,    -1.6383743000000095,     00000000000000000000000000000000);
-INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(1, 1508676039,    48.1151495,    -1.6383743000000095,     11111111111111111111111111111111);
-INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(1, 1508666400,    48.862725,     2.257592000000018,       22222222222222222222222222222222);
-INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(1, 1508676039,    48.862725,     2.257592000000018,       33333333333333333333333333333333);
-INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(1, 1521034928,    48.1151495,    -1.6383743000000095,     44444444444444444444444444444444);
-INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(1, 1521034928,    48.1151495,    -1.6383743000000095,     55555555555555555555555555555555);
-INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(1, 1521034928,    48.862725,     2.257592000000018,       66666666666666666666666666666666);
-INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(1, 1521034928,    48.862725,     2.257592000000018,       77777777777777777777777777777777);
+INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(1, 1508666400,48.1151495,   -1.74807547, 00000000000000000000000000000000);
+INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(1, 1508676039,48.08820964,  -1.61923693, 11111111111111111111111111111111);
+INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(2, 1508666400,48.15933586,  -1.5720499,  22222222222222222222222222222222);
+INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(1, 1508676039,48.0963606,   -1.77338978, 33333333333333333333333333333333);
+INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(2, 1521034928,48.12025532,  -1.73166387, 44444444444444444444444444444444);
+INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(1, 1521034928,48.11425892,  -1.71566634, 55555555555555555555555555555555);
+INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(2, 1521034928,48.12941941,  -1.62325646, 66666666666666666666666666666666);
+INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(1, 1521034928,48.06546228,  -1.63183894, 77777777777777777777777777777777);
+INSERT INTO meta_mesures(id_hub,date,gps_lat,gps_long,hash) VALUES(2, 1521034928,48.12870096,  -1.77417406, 88888888888888888888888888888888);
 
 /* On peuple mesures */
 INSERT INTO mesures(id_capteur,id_meta,valeur) VALUES(1,1,156.6556);
