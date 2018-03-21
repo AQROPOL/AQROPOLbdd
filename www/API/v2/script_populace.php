435e70d34d3e8a968c65d312ba7fc0f93771c492
Thibault Leclair <thibault.leclair@gmail.com>
2018-03-21 14:44:08 +0100

    fix populace v1

--- www/API/v2/script_populace.php
+++ www/API/v2/script_populace.php
@@ -8,13 +8,13 @@ for ($i = 1; $i < 1000; $i++) {
      $aleaLong = rand(-144, -184);
      $aleaLong = $aleaLong / 100;
 
-     $date = new DateTime();
-     $dateTmp = $date->getTimestamp();
+     $dateTmp = new DateTime()->getTimestamp();
      $randIdHub = rand(1,2);
      $hash = hash('sha256', $dateTmp);
      $bool = $stmt_insertMetaMesures->execute(array(':id_hub' => $randIdHub, ':date' => $dateTmp, ':gps_long' => $aleaLong, ':gps_lat' => $aleaLat, ":hash" => $hash));
-     if ($bool) echo "Success\n";
-     else {
+     
+     if ($bool === true) echo "Success\n";
+     else if ($bool === false) {
           echo "<br>PDO::errorInfo():";
           print_r($db->errorInfo());
      }
@@ -24,8 +24,8 @@ for ($i = 1; $i < 1000; $i++) {
      $randValeur = rand(0,10000);
 
      $bool = $stmt_insertMesures->execute(array(':id_capteur' => $randIdCapteur, ':id_meta' => $i, ':valeur' => $randValeur));
-     if ($bool) echo "Success\n";
-     else {
+     if ($bool === true) echo "Success\n";
+     else if ($bool === false) {
           echo "<br>PDO::errorInfo():";
           print_r($db->errorInfo());
      }
