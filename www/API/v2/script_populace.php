f45beff8194cf61b4db172db8d9a725063143227
Thibault Leclair <thibault.leclair@gmail.com>
2018-03-21 15:13:44 +0100

    fix populace v6

--- www/API/v2/script_populace.php
+++ www/API/v2/script_populace.php
@@ -2,34 +2,32 @@
 
 require 'db_access.php';
 
-for ($i = 1; $i < 1000; $i++) {
+for ($i = 1; $i < 30000; $i++) {
      $aleaLat = rand(480200, 482000);
      $aleaLat = $aleaLat / 10000;
      $aleaLong = rand(-14400, -18400);
      $aleaLong = $aleaLong / 10000;
 
-     //$date = new DateTime();
-     //$dateTmp = $date->getTimestamp();
      $dateTmp = round(microtime(true) * 1000);
      $randIdHub = rand(1,2);
      $hash = hash('sha256', $dateTmp);
      $bool = $stmt_insertMetaMesures->execute(array(':id_hub' => $randIdHub, ':date' => $dateTmp, ':gps_long' => $aleaLong, ':gps_lat' => $aleaLat, ":hash" => $hash));
      
-     if ($bool === true) echo "Success\n";
+     /*if ($bool === true) echo "Success\n";
      else if ($bool === false) {
           echo "<br>PDO::errorInfo():";
           print_r($db->errorInfo());
-     }
+     }*/
 
 
      $randIdCapteur = rand(1,4);
      $randValeur = rand(0,10000);
 
      $bool = $stmt_insertMesures->execute(array(':id_capteur' => $randIdCapteur, ':id_meta' => $i, ':valeur' => $randValeur));
-     if ($bool === true) echo "Success\n";
+     /*if ($bool === true) echo "Success\n";
      else if ($bool === false) {
           echo "<br>PDO::errorInfo():";
           print_r($db->errorInfo());
-     }
+     }*/
 }
  ?>
