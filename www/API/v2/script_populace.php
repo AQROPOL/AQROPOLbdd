62e00fa6e7e99f86ff7f4ded3ad6a0d64a1047ae
Thibault Leclair <thibault.leclair@gmail.com>
2018-03-21 14:56:58 +0100

    fix populace v3

--- www/API/v2/script_populace.php
+++ www/API/v2/script_populace.php
@@ -8,8 +8,9 @@ for ($i = 1; $i < 1000; $i++) {
      $aleaLong = rand(-144, -184);
      $aleaLong = $aleaLong / 100;
 
-     $date = new DateTime();
-     $dateTmp = $date->getTimestamp();
+     //$date = new DateTime();
+     //$dateTmp = $date->getTimestamp();
+     $dateTmp = round(microtime(true) * 1000);
      $randIdHub = rand(1,2);
      $hash = hash('sha256', $dateTmp);
      $bool = $stmt_insertMetaMesures->execute(array(':id_hub' => $randIdHub, ':date' => $dateTmp, ':gps_long' => $aleaLong, ':gps_lat' => $aleaLat, ":hash" => $hash));
