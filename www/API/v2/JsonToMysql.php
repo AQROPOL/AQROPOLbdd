2d040ae68a6a501d822a2728e0453c783c9dae18
NoureddineK <noureddine.kadri05@gmail.com>
2018-04-04 17:25:56 +0200

    test json

--- www/API/v2/JsonToMysql.php
+++ www/API/v2/JsonToMysql.php
@@ -9,7 +9,7 @@ echo("POST Recu !\n");
 		$json_data = file_get_contents("json_data.json");
 //if(!empty($_POST["file"])){
 echo("Test Recu !\n");
-		$data=json_decode($json_data ,true);
+		$data=json_decode($json_data);
 
 		$id_nuc=0;
 		$id_capteur=0;
