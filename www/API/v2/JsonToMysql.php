32512271a49fd622f9c4ffc6ac6dbdb94d9f1f58
NoureddineK <noureddine.kadri05@gmail.com>
2018-04-04 15:43:01 +0200

    test json

--- www/API/v2/JsonToMysql.php
+++ www/API/v2/JsonToMysql.php
@@ -1,13 +1,13 @@
 <?php
-	require 'db_access.php';
+	require "db_access.php";
 //Mon URL http://pilic27.irisa.fr/API/v2/JsonToMySql.php
 
 	header("Content-type: application/json");
 echo "POST Request\n";
-	if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+	if ($_SERVER["REQUEST_METHOD"] == "POST"){
 echo("POST Recu !\n");
-		$json_data = file_get_contents('json_data.json');
-//if(!empty($_POST['file'])){
+		$json_data = file_get_contents("json_data.json");
+//if(!empty($_POST["file"])){
 echo("Test Recu !\n");
 		$data=json_decode($json_data ,true);
 
@@ -15,49 +15,53 @@ echo("Test Recu !\n");
 		$id_capteur=0;
 		$id_meta=0;
 		$nuc = $data["nuc"];
-		$stmt_insertHubs -> bindParam(':name',$nuc);
+		$stmt_insertHubs -> bindParam(":name",$nuc);
 		$stmt_insertHubs -> execute();
 		echo ("Insertion Hubs\n");
 
 	while ($row = $stmt_insertHubs->fetch(PDO::FETCH_ASSOC)) {
-		$id_nuc = $row['id'];
-		echo "Recuperation Id nuc\n";
+		$id_nuc = $row["id"];
+		echo "Recuperation Id nuc : \n".$id_nuc;
 		}
 
 		$mesures=$data["mesures"];
 		$max = sizeof($mesures);
+		echo ("taille des mesures : ".$max);
 
 
 	for ($i = 0; $i < $max; $i++) {
 
-		$stmt_insertCapteurs -> bindParam(':id_hub',$id_nuc);
-		$stmt_insertCapteurs -> bindParam(':name',$mesures[i]["capteur"]);
-		$stmt_insertCapteurs -> bindParam(':type',$mesures[i]["type"]);
+		$stmt_insertCapteurs -> bindParam(":id_hub",$id_nuc);
+		$stmt_insertCapteurs -> bindParam(":name",$mesures[i]["capteur"]);
+		$stmt_insertCapteurs -> bindParam(":type",$mesures[i]["type"]);
 		$stmt_insertCapteurs -> execute();
 		echo "Insertion capteurs\n";
 
 	while ($row = $stmt_insertCapteurs ->fetch(PDO::FETCH_ASSOC)) {
-		$id_capteur = $row['id'];
+		$id_capteur = $row["id"];
+		echo "Recuperation Id capteur : \n".$id_capteur;
 		}
 
-		$stmt_insertMetaMesures ->bindParam(':id_hub',$id_nuc);
-		$stmt_insertMetaMesures ->bindParam(':date',$mesures[i]["date"]);
-		$stmt_insertMetaMesures ->bindParam(':gps_lat',$mesures[i]["lat"]);
-		$stmt_insertMetaMesures ->bindParam(':gps_long',$mesures[i]["long"]);
-		$stmt_insertMetaMesures ->bindParam(':hash',$mesures[i]["hash"]);
+
+		$stmt_insertMetaMesures ->bindParam(":id_hub",$id_nuc);
+		$stmt_insertMetaMesures ->bindParam(":date",$mesures[i]["date"]);
+		$stmt_insertMetaMesures ->bindParam(":gps_lat",$mesures[i]["lat"]);
+		$stmt_insertMetaMesures ->bindParam(":gps_long",$mesures[i]["long"]);
+		$stmt_insertMetaMesures ->bindParam(":hash",$mesures[i]["hash"]);
 		$stmt_insertMetaMesures -> execute();
 		echo "Insertion Meta Mesures\n";
 
 	 while ($row = $stmt_insertMetaMesures ->fetch(PDO::FETCH_ASSOC)) {
-		$id_meta = $row['id'];
+		$id_meta = $row["id"];
+			echo "Recuperation Id meta : \n".$id_meta;
 
 		}
 
 
 
-		$stmt_insertMesures->bindParam(':id_capteur',$id_capteur);
-		$stmt_insertMesures->bindParam(':id_meta',$id_meta);
-		$stmt_insertMesures->bindParam(':valeur',$mesures[i]["valeur"]);
+		$stmt_insertMesures->bindParam(":id_capteur",$id_capteur);
+		$stmt_insertMesures->bindParam(":id_meta",$id_meta);
+		$stmt_insertMesures->bindParam(":valeur",$mesures[i]["valeur"]);
 		$stmt_insertMesures->execute();
 		echo "Insertion Mesures\n";
 
