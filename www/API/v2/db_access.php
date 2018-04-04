a833b01a61f10d1a86e09f64007a1bbe0081cce9
Thibault Leclair <thibault.leclair@gmail.com>
2018-04-04 15:29:32 +0200

    Ajout de commentaires

--- www/API/v2/db_access.php
+++ www/API/v2/db_access.php
@@ -1,4 +1,6 @@
 <?php
+	/* Fichier relatif a la connexion a la base de donnees et aux requetes SQL preparees */
+
 	require 'config.php';
 
 	try {
@@ -6,24 +8,40 @@
 	} catch (PDOException $e) {
 		echo 'Connexion échouée : '.$e->getMessage();
 	}
-	if(isset($user)){
+	if (isset($user)) {
 		try {
 			$db_read = new PDO($dsn,$user['username'],$user['password']);
 		} catch (PDOException $e) {
 			echo 'Connexion échouée : '.$e->getMessage();
 		}
 	}
-	else
+	else {
 		$db_read = $db;
+	}
+	/* Insertion de donnees dans la table mesures */
 	$stmt_insertMesures = $db->prepare('INSERT INTO mesures (id_capteur, id_meta, valeur) VALUES (:id_capteur, :id_meta, :valeur);');
+	
+	/* Insertion de donnees dans la table meta_mesures */
 	$stmt_insertMetaMesures = $db->prepare('INSERT INTO meta_mesures (id_hub, date, gps_long, gps_lat, hash) VALUES (:id_hub, :date, :gps_long, :gps_lat, :hash);');
+	
+	/* Insertion de donnees dans la table capteurs */
 	$stmt_insertCapteurs = $db->prepare('INSERT INTO capteurs (id_hub, name, type) VALUES (:id_hub, :name, :type);');
+	
+	/* Insertion de donnees dans la table hubs */
 	$stmt_insertHubs = $db->prepare('INSERT INTO hubs (name) VALUES (:name);');
 
+	/* Lecture de la table mesures */
 	$stmt_readMesure = $db_read->prepare('SELECT m.id,m.id_capteur,m.id_meta,m.valeur,c.type FROM mesures m,capteurs c WHERE m.id_capteur = c.id;');
+	
+	/* Lecture de la table meta_mesures */
 	$stmt_readMeta = $db_read->prepare('SELECT id,id_hub,date,gps_lat,gps_long FROM meta_mesures;');
-	/*$stmt_readMeta = $db_read->prepare('SELECT id,id_hub,date,gps_lat,gps_long FROM meta_mesures; WHERE date = :date');*/
+	
+	/* Lecture de la table capteurs */
 	$stmt_readCapteur = $db_read->prepare('SELECT id,id_hub,type FROM capteurs;');
+	
+	/* Lecture de la table hubs */
 	$stmt_readHub = $db_read->prepare('SELECT id,name FROM hubs;');
+	
+	/* Lecture du dernier hash */
 	$stmt_readLastHash = $db_read->prepare('SELECT id_hub, hash FROM meta_mesures m1 WHERE date = (SELECT max(date) FROM meta_mesures m2 WHERE m1.id_hub = m2.id_hub)');
 ?>
--- www/API/v2/script_populace.php
+++ www/API/v2/script_populace.php
@@ -2,7 +2,10 @@
 
 require 'db_access.php';
 
+/* Script PHP pour remplir la base de donnees pour des tests */
+
 for ($i = 1; $i < 1000; $i++) {
+
      $aleaLat = rand(480200, 482000);
      $aleaLat = $aleaLat / 10000;
      $aleaLong = rand(-14400, -18400);
@@ -13,21 +16,10 @@ for ($i = 1; $i < 1000; $i++) {
      $hash = hash('sha256', $dateTmp);
      $bool = $stmt_insertMetaMesures->execute(array(':id_hub' => $randIdHub, ':date' => $dateTmp, ':gps_long' => $aleaLong, ':gps_lat' => $aleaLat, ":hash" => $hash));
      
-     /*if ($bool === true) echo "Success\n";
-     else if ($bool === false) {
-          echo "<br>PDO::errorInfo():";
-          print_r($db->errorInfo());
-     }*/
-
-
      $randIdCapteur = rand(1,4);
      $randValeur = rand(0,10000);
 
      $bool = $stmt_insertMesures->execute(array(':id_capteur' => $randIdCapteur, ':id_meta' => $i, ':valeur' => $randValeur));
-     /*if ($bool === true) echo "Success\n";
-     else if ($bool === false) {
-          echo "<br>PDO::errorInfo():";
-          print_r($db->errorInfo());
-     }*/
+
 }
  ?>
