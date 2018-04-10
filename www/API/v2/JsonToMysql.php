d5c4813c91768354c1b2111a151416871e0ad13c
NoureddineK <noureddine.kadri05@gmail.com>
2018-04-10 14:53:00 +0200

    Post/Get OK

--- www/API/v2/JsonToMysql.php
+++ www/API/v2/JsonToMysql.php
@@ -3,11 +3,9 @@
 	header("Content-type: application/json");
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST"){
-			echo "POST Request\n";
-			echo("POST Recu !\n");
+		if (isset($_POST["file"])) {
 			if(!empty($_POST["file"])){
-				echo("Test Recu !\n");
-				//$data=json_decode($json_data,true);
+				//Decode Json Android
 				$data=json_decode($_POST["file"],true);
 				$id_nuc;
 				$id_capteur;
@@ -24,7 +22,6 @@
 				//Tableau de Mesures
 				$mesures=$data["mesures"];
 				$max = sizeof($mesures);
-				echo ("taille des mesures : ".$max."\n");
 
 				//Insertion capteurs
 				for ($i = 0; $i < $max; $i++) {
@@ -32,11 +29,11 @@
 					$stmt_insertCapteurs -> bindParam(":name",$mesures[$i]["capteur"]);
 					$stmt_insertCapteurs -> bindParam(":type",$mesures[$i]["type"]);
 					$stmt_insertCapteurs -> execute();
+
 					// Recuperer Id Capteur
 					$capt = $db->prepare('SELECT Max(id) as "max" FROM capteurs');
 					$capt->execute();
 					$id_capteur = $capt->fetchColumn();
-					print("Id capteur = $id_capteur\n");
 
 					//Insertion Meta Mesures
 					$stmt_insertMetaMesures ->bindParam(":id_hub",$id_nuc);
@@ -45,32 +42,34 @@
 					$stmt_insertMetaMesures ->bindParam(":gps_long",$mesures[$i]["long"]);
 					$stmt_insertMetaMesures ->bindParam(":hash",$mesures[$i]["hash"]);
 					$stmt_insertMetaMesures -> execute();
+
 					//Recuperer Id Meta_Mesures
 					$meta = $db->prepare('SELECT Max(id) as "max" FROM meta_mesures');
 					$meta->execute();
 					$id_meta = $meta->fetchColumn();
-					print("Id Meta = $id_meta\n");
+
 					//Insertion Mesures
 					$stmt_insertMesures->bindParam(":id_capteur",$id_capteur);
 					$stmt_insertMesures->bindParam(":id_meta",$id_meta);
 					$stmt_insertMesures->bindParam(":valeur",$mesures[$i]["valeur"]);
 					$stmt_insertMesures->execute();
+					}
 				}
-			}
 				else {
 					echo " POST File Empty !";
 				}
+			}
+
 		}
 		else {
 			if ($_SERVER["REQUEST_METHOD"] == "GET"){
-				echo "GET Request \n";
+				// Encode Json Dernier Hash/Capt
 				$hash_capt = $db->prepare('SELECT capteurs.id,meta_mesures.hash,Max(date)
 																	from meta_mesures, capteurs, mesures
 																	where capteurs.id = mesures.id_capteur
 																	and meta_mesures.id=mesures.id_meta
 																	group by capteurs.id');
 				$hash_capt->execute();
-				//$id_meta = $meta->fetchColumn();
 				$tabHash = $hash_capt->fetchAll();
 				$tabCapt_Hash = array();
 				$i=0;
@@ -78,8 +77,8 @@
 					$tabCapt_Hash[$i]['id_capt']=$ligneHash['id'];
 					$tabCapt_Hash[$i]['hash']=$ligneHash['hash'];
 					$i++;
-			}
+					}
 				echo json_encode($tabCapt_Hash, JSON_NUMERIC_CHECK);
+			}
 		}
-}
 ?>
