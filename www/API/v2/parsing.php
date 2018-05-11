0dab50412351be6363c0e12e3ebf25fe6fb75ad1
NoureddineK <noureddine.kadri05@gmail.com>
2018-05-11 16:58:25 +0200

    Test nouveau pars

---
+++ www/API/v2/parsing.php
@@ -0,0 +1,94 @@
+<?php
+	require "db_access.php";
+	header("Content-type: application/json");
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST"){
+		/* Gestion de la requete d'insertion des donnees venant d'Android
+		 * Parser la variable du POST
+		 * Utilisation du format JSON
+		*/
+		$json_source = file_get_contents('Data.json');
+			if(!empty($json_source)){
+				//Decode Json Android
+				$data=json_decode($json_source,true);
+				$id_nuc;
+				$id_capteur;
+				$id_meta;
+
+				$mesures=$data["measures"];
+				$Size_Mesures = sizeof($mesures);
+
+				for ($i = 0; $i < $Size_Mesures; $i++) {
+					$nuc = $data["token"];
+					//Insertion Hubs
+					$stmt_insertHubs -> bindParam(":name",$nuc);
+					$stmt_insertHubs -> execute();
+					// Recuperer Id nuc
+					$hub = $db->prepare('SELECT id FROM hubs where name = '.$nuc);
+					$hub->execute();
+					$id_nuc = $hub->fetchColumn();
+
+					$stmt_insertCapteurs -> bindParam(":id_hub",$id_nuc);
+					$stmt_insertCapteurs -> bindParam(":name",$mesures[$i]["name"]);
+					$stmt_insertCapteurs -> bindParam(":type",$mesures[$i]["type"]);
+					$stmt_insertCapteurs -> execute();
+
+					// Recuperer Id Capteur
+					$capt = $db->prepare('SELECT Max(id) as "max" FROM capteurs');
+					$capt->execute();
+					$id_capteur = $capt->fetchColumn();
+
+					//Insertion Meta Mesures
+					$stmt_insertMetaMesures ->bindParam(":id_hub",$id_nuc);
+					$stmt_insertMetaMesures ->bindParam(":date",$mesures[$i]["timestamp"]);
+					//$stmt_insertMetaMesures ->bindParam(":gps_lat",$mesures[$i]["lat"]);
+					//$stmt_insertMetaMesures ->bindParam(":gps_long",$mesures[$i]["long"]);
+					$stmt_insertMetaMesures ->bindParam(":hash",$mesures[$i]["hash"]);
+					$stmt_insertMetaMesures -> execute();
+
+					//Recuperer Id Meta_Mesures
+					$meta = $db->prepare('SELECT Max(id) as "max" FROM meta_mesures');
+					$meta->execute();
+					$id_meta = $meta->fetchColumn();
+
+					//Insertion Mesures
+					$stmt_insertMesures->bindParam(":id_capteur",$id_capteur);
+					$stmt_insertMesures->bindParam(":id_meta",$id_meta);
+					$stmt_insertMesures->bindParam(":valeur",$mesures[$i]["value"]);
+					$stmt_insertMesures->execute();
+					}
+
+
+				//Tableau de Mesures
+				//Insertion capteurs
+
+				}
+				else {
+					echo " POST File Empty !";
+				}
+
+
+		}
+		else {
+			/* Recuperation des derniers hash ainsi que leurs Id et les envoye vers Android
+			*/
+			if ($_SERVER["REQUEST_METHOD"] == "GET"){
+				// Encode Json Dernier Hash/Capt
+				$hash_capt = $db->prepare('SELECT hubs.id,meta_mesures.hash,Max(date)
+																	from meta_mesures, hubs, mesures
+																	where hubs.id = meta_mesures.id_hub
+																	and meta_mesures.id=mesures.id_meta
+																	group by hubs.id');
+				$hash_capt->execute();
+				$tabHash = $hash_capt->fetchAll();
+				$tabCapt_Hash = array();
+				$i=0;
+				foreach ($tabHash as &$ligneHash) {
+					$tabCapt_Hash[$i]['id_hub']=$ligneHash['id'];
+					$tabCapt_Hash[$i]['hash']=$ligneHash['hash'];
+					$i++;
+					}
+				echo json_encode($tabCapt_Hash, JSON_NUMERIC_CHECK);
+			}
+		}
+?>
